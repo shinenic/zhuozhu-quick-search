319b3b6d51284c33d6a8bd942c9075a3172a391e
Kadenz <galadiya41@gmail.com>
2019-10-05 15:02:17 +0800

    Updates

---
+++ precache-manifest.9ce3e2f15c717e9fb337d588bda33d35.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c5a7360aa32855e366dcfe5b02cfdf",
+    "url": "/zhuozhe-quick-search/index.html"
+  },
+  {
+    "revision": "5e5266c09fa5e05b49c2",
+    "url": "/zhuozhe-quick-search/static/css/main.14ee25a3.chunk.css"
+  },
+  {
+    "revision": "06474052d1151c235a86",
+    "url": "/zhuozhe-quick-search/static/js/2.baf3b1b5.chunk.js"
+  },
+  {
+    "revision": "5e5266c09fa5e05b49c2",
+    "url": "/zhuozhe-quick-search/static/js/main.3dbd9ad3.chunk.js"
+  },
+  {
+    "revision": "544c2d9868174dba5e48",
+    "url": "/zhuozhe-quick-search/static/js/runtime-main.9cd7e9b1.js"
+  },
+  {
+    "revision": "718029c6e0b6b4eaee9823e8d6a555ca",
+    "url": "/zhuozhe-quick-search/static/media/Pusheen1.718029c6.png"
+  },
+  {
+    "revision": "f460a07882c941e6582f9fa6e4842677",
+    "url": "/zhuozhe-quick-search/static/media/Pusheen2.f460a078.png"
+  },
+  {
+    "revision": "1f9a08df879aae5aadc0f830c3c0fb29",
+    "url": "/zhuozhe-quick-search/static/media/Pusheen3.1f9a08df.png"
+  },
+  {
+    "revision": "1f018c7e1b26506ee466e8b4cddba216",
+    "url": "/zhuozhe-quick-search/static/media/五月天.1f018c7e.jpg"
+  },
+  {
+    "revision": "159d6810640e9ed707e4c076719a98f6",
+    "url": "/zhuozhe-quick-search/static/media/周杰倫.159d6810.jpg"
+  },
+  {
+    "revision": "e65a45c0d44b7eaf5e07ec2c609b1f30",
+    "url": "/zhuozhe-quick-search/static/media/張信哲.e65a45c0.jpg"
+  },
+  {
+    "revision": "8db2ea58434a317610213cb926b66688",
+    "url": "/zhuozhe-quick-search/static/media/張學友.8db2ea58.jpg"
+  },
+  {
+    "revision": "cabe9fec15545909f80cb2e6e8824c19",
+    "url": "/zhuozhe-quick-search/static/media/張惠妹.cabe9fec.jpg"
+  },
+  {
+    "revision": "fa9f0863b516af5f1b45d74449bb20c3",
+    "url": "/zhuozhe-quick-search/static/media/林俊傑.fa9f0863.jpg"
+  },
+  {
+    "revision": "0503685f767fd2f889dc79e3a0f6e0e1",
+    "url": "/zhuozhe-quick-search/static/media/林志炫.0503685f.jpg"
+  },
+  {
+    "revision": "93158cfb8d82672d4872d35f755d7b56",
+    "url": "/zhuozhe-quick-search/static/media/林憶蓮.93158cfb.jpg"
+  },
+  {
+    "revision": "3caefc09056a0216a6ff2e00200a7ca5",
+    "url": "/zhuozhe-quick-search/static/media/梁靜茹.3caefc09.jpg"
+  },
+  {
+    "revision": "221857b5f693d98c8b4149b1aa768157",
+    "url": "/zhuozhe-quick-search/static/media/江蕙.221857b5.jpg"
+  },
+  {
+    "revision": "408fad54712e1fd1ebb782caf5d0766a",
+    "url": "/zhuozhe-quick-search/static/media/王力宏.408fad54.jpg"
+  },
+  {
+    "revision": "5b8819fedd5643919636a667f1959bf2",
+    "url": "/zhuozhe-quick-search/static/media/王菲.5b8819fe.jpg"
+  },
+  {
+    "revision": "8e30ab765a51a6539e400144cba2d42a",
+    "url": "/zhuozhe-quick-search/static/media/莫文蔚.8e30ab76.jpg"
+  },
+  {
+    "revision": "057dbde2eee23e2eabe5fa04781f720c",
+    "url": "/zhuozhe-quick-search/static/media/蔡依林.057dbde2.jpg"
+  },
+  {
+    "revision": "e55e71c74c03080bf6c9358babd1eec3",
+    "url": "/zhuozhe-quick-search/static/media/許茹芸.e55e71c7.jpg"
+  },
+  {
+    "revision": "239aadd13c4ad4d1bc6259cc957feb27",
+    "url": "/zhuozhe-quick-search/static/media/辛曉琪.239aadd1.jpg"
+  },
+  {
+    "revision": "9072040de42db0d5dcee5551e3bb0b8b",
+    "url": "/zhuozhe-quick-search/static/media/陳昇.9072040d.jpg"
+  }
+]);
