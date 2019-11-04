06c94b907a4d6044b1440c9c7c5b7d9f71ac1dd5
tim <galadiya41@gmail.com>
2019-11-04 22:47:05 +0800

    修改 router 規則

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import Reducer from './reducers/index';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
-import { HashRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(Reducer, composeEnhancers(applyMiddleware(thunk)))
